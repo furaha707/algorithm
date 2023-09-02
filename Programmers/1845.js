891c4c85acda72a9ddca32efaa10f341edad87f5
furaha707 <zxcvyein950823@gmail.com>
2023-09-02 10:53:59 +0900

    programmers: 1845

--- Programmers/1845.js
+++ Programmers/1845.js
@@ -1,8 +1,10 @@
 function solution(nums) {
   var answer = 0;
   
+  //  배열에서 중복되는 숫자 제거
   const uniqueNumbers = new Set(nums);
   const uniqueCount = uniqueNumbers.size;
+
   const halfLength = nums.length / 2;
 
   if (uniqueCount <= halfLength) {
@@ -10,7 +12,6 @@ function solution(nums) {
   } else {
     return halfLength;
   }
-  
 }
 
 solution([3,3,3,2,2,4])
